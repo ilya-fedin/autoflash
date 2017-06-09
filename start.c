c56addc6caf81838eccb1f9efba8d752632bfe16
Ilya Fedin <fedin-ilja2010@ya.ru>
2017-06-09 15:06:59 +0400

    Еще упрощение

--- start.c
+++ start.c
@@ -6,16 +6,14 @@
 
 int main(int argc, char** argv) {
 	char* arg0 = "sh.exe main.sh";
-	char* arg_open = " \"";
-	char* arg_close = "\"";
 	char* command = malloc(strlen(arg0) + 1);
 	strcpy(command, arg0);
 	int returncode = 0;
 
 	if(argc > 1) {
 		for(int i = 1; i < argc; i++) {
-			command = realloc(command, strlen(command) + strlen(arg_open) + strlen(argv[i]) + strlen(arg_close) + 1);
-			sprintf(command, "%s%s%s%s", command, arg_open, argv[i], arg_close);
+			command = realloc(command, strlen(command) + strlen(argv[i]) + 4);
+			sprintf(command, "%s \"%s\"", command, argv[i]);
 		}
 	}
 
