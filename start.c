b633d11390feed469e196498bc11128583ddc335
Ilya Fedin <fedin-ilja2010@ya.ru>
2017-06-09 14:39:06 +0400

    Уменьшение кода

--- start.c
+++ start.c
@@ -1,35 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <libgen.h>
 #include <windows.h>
 
 int main(int argc, char** argv) {
 	char* arg0 = "sh.exe main.sh";
+	char* arg_open = " \"";
+	char* arg_close = "\"";
 	char* command = malloc(strlen(arg0) + 1);
 	strcpy(command, arg0);
 	int returncode = 0;
 
 	if(argc > 1) {
-		char* arguments = malloc(strlen(" \"") + 1);
-		strcpy(arguments, " \"");
-		arguments = realloc(arguments, strlen(arguments) + strlen(argv[1]) + 1);
-		strcat(arguments, argv[1]);
-		arguments = realloc(arguments, strlen(arguments) + strlen("\"") + 1);
-		strcat(arguments, "\"");
+		char* arguments = malloc(strlen(arg_open) + strlen(argv[1]) + strlen(arg_close) + 1);
+		sprintf(arguments, "%s%s%s", arg_open, argv[1], arg_close);
 		if(argc > 2) {
 			for(int i = 2; i < argc; i++) {
-				arguments = realloc(arguments, strlen(arguments) + strlen(" \"") + 1);
-				strcat(arguments, " \"");
-				arguments = realloc(arguments, strlen(arguments) + strlen(argv[i]) + 1);
-				strcat(arguments, argv[i]);
-				arguments = realloc(arguments, strlen(arguments) + strlen("\"") + 1);
-				strcat(arguments, "\"");
+				arguments = realloc(arguments, strlen(arguments) + strlen(arg_open) + strlen(argv[i]) + strlen(arg_close) + 1);
+				sprintf(arguments, "%s%s%s%s", arguments, arg_open, argv[i], arg_close);
 			}
 		}
-		command = realloc(command, strlen(command) + strlen(" ") + 1);
-		strcat(command, " ");
-		command = realloc(command, strlen(command) + strlen(arguments) + 1);
-		strcat(command, arguments);
+		command = realloc(command, strlen(command) + strlen(" ") + strlen(arguments) + 1);
+		sprintf(command, "%s %s", command, arguments);
 		free(arguments);
 	}
 
